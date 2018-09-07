4174625075c860c56468d4261e1a293d83f9794c
Ahmzyjazzy <ahmed.olanrewaju@hts.com.ng>
2018-09-07 22:35:38 +0100

    feat: initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DP</title>
+	<link rel="stylesheet" type="text/css" href="src/css/style.css">
+</head>
+<body>
+
+	<header>
+		<h1>KwaraBuild</h1>
+	</header>
+
+	<main>
+
+		<section>
+			
+		</section>
+
+		<section>
+			
+		</section>
+		
+	</main>
+
+	<footer>
+		<p>Copyright</p>
+	</footer>
+
+</body>
+</html>
