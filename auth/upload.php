691fce8659bed22a90788fabaeff923e27c1f3fe
Ahmzyjazzy <ahmed.olanrewaju@hts.com.ng>
2018-09-08 22:39:44 +0100

    feat: add text to image and formatting

--- auth/upload.php
+++ auth/upload.php
@@ -4,19 +4,17 @@ include_once("DP.php");
 
 $DP = new DISPLAY_PICTURE();
 
-$txt = "Ahmed Olanrewaju Olayinka jhhfdjjhdf";
-
 $uploadDir = "../uploads/";
-$moveToDir = "../uploads/thumbnail/";
+$thumbnailDir = "../uploads/thumbnail/";
 
-$final_destination = "../uploads/dp/";
+$destination_folder_for_dp = "../uploads/dp/";
 $frameImg = "../src/img/frame.jpeg";
 
 // image dimension variable
 $img_width = 170;
 $img_height = 126;
 
-// image position
+// image position right:bottom
 $margin_right = 143;
 $margin_bottom = 148;
 
@@ -27,7 +25,7 @@ if(isset($_FILES["avatar"]["type"])) {
     $file_extension = end($temporary);        
 
     $time = $_POST['timestamp'];
-    $username = $_POST['username'];
+    $txt = $_POST['fullname'];
     $newfilename = $time .".".$file_extension;
     
     if ((($_FILES["avatar"]["type"] == "image/png") || ($_FILES["avatar"]["type"] == "image/jpg") || ($_FILES["avatar"]["type"] == "image/jpeg")) && ($_FILES["avatar"]["size"] <= 2000000) && in_array($file_extension, $validextensions)) {
@@ -47,16 +45,27 @@ if(isset($_FILES["avatar"]["type"])) {
             $sourcePath = $_FILES["avatar"]["tmp_name"]; // Storing source path of the file in a variable
             $targetPath = $uploadDir.$newfilename; // Target path where file is to be stored
             
-            if(move_uploaded_file($sourcePath,$targetPath) /*Moving Uploaded file*/ ){
+            if(move_uploaded_file($sourcePath,$targetPath) /*Save Uploaded file*/ ){
 
               //create dp instance 
               $dp = new DISPLAY_PICTURE($newfilename);
 
               //create thumnail
-              $dp->createThumbnail($newfilename, $img_width, $img_height, $uploadDir, $moveToDir);
+              if($dp->createThumbnail($newfilename, $img_width, $img_height, $uploadDir, $thumbnailDir)){
 
-              //merge picture
-              $dp->mergeImage($txt, $frameImg, $moveToDir.$newfilename, $margin_right, $margin_bottom, $final_destination);
+                //merge picture
+                if($dp->mergeImage($txt, $frameImg, $thumbnailDir.$newfilename, $margin_right, $margin_bottom, $destination_folder_for_dp)){
+                  
+                  //send merge picture to browser
+                  echo "Dp created successfully";
+
+                }else{
+                  echo "Image processing failed, please try again.";
+                }
+
+              }else{
+                echo "Image processing failed, please try again.";
+              }              
                 
             }else{
               echo "Image processing failed, please try again.";
